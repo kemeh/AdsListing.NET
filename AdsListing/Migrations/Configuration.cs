bfd53a9f7f4016a80e611720bd0f38751bc007fc
kemeh <knm1988@gmail.com>
2017-04-16 17:09:49 +0300

    Upload additional files

---
+++ AdsListing/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace AdsListing.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AdsListing.Models.AdsListingDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(AdsListing.Models.AdsListingDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ AdsListing/Models/AdsListingDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AdsListing.Models
+{
+    public class AdsListingDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AdsListingDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static AdsListingDbContext Create()
+        {
+            return new AdsListingDbContext();
+        }
+    }
+}
