0e3075cf96345d7ee24ea1e815f0f30c591ed6b7
kemeh <knm1988@gmail.com>
2017-04-25 18:48:28 +0300

    Implemented filtering by category, location and keyword

--- AdsListing/Controllers/HomeController.cs
+++ AdsListing/Controllers/HomeController.cs
@@ -13,17 +13,20 @@ namespace AdsListing.Controllers
     {
         public ActionResult Index()
         {
-            using (var database = new AdsListingDbContext())
-            {
-                var ads = database
-                    .Ads
-                    .Include(a => a.Photos)
-                    .Include(a => a.Location)
-                    .ToList();
+            //using (var database = new AdsListingDbContext())
+            //{
+            //    var ads = database
+            //        .Ads
+            //        .Include(a => a.Photos)
+            //        .Include(a => a.Location)
+            //        .Take(26)
+            //        .ToList();
 
-                return View(ads);
-            }
-            
+            //    return View(ads);
+            //}
+
+            return RedirectToAction("ListAds");
+
         }
 
         public ActionResult ListCategories()
@@ -40,18 +43,73 @@ namespace AdsListing.Controllers
             }
         }
 
-        public ActionResult ListAds(int? categoryId)
+        public ActionResult ListAds(                       
+            int page = 1,
+            string category = null,
+            string locations = null,
+            string search = null
+            )
         {
-            if (categoryId == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
             using (var database = new AdsListingDbContext())
             {
-                var ads = database
-                    .Ads
-                    .Where(a => a.CategoryId == categoryId)
+                var LocationsList = database
+                .Locations
+                .OrderBy(l => l.Name)
+                .ToList();
+
+                var locationNames = new Dictionary<string, int>();
+
+                foreach (var loc in LocationsList)
+                {
+                    locationNames[loc.Name] = loc.Id;
+                }
+
+                ViewBag.Locations = new SelectList(locationNames.Keys);
+
+                var CategoriesList = database
+                .Categories
+                .OrderBy(l => l.Name)
+                .ToList();
+
+                var categoryNames = new Dictionary<string, int>();
+
+                foreach (var cat in CategoriesList)
+                {
+                    categoryNames[cat.Name] = cat.Id;
+                }
+
+                ViewBag.Category = new SelectList(categoryNames.Keys);
+
+                var pageSize = 5;
+
+                var adsQuery = database.Ads.AsQueryable();
+
+                if (!string.IsNullOrEmpty(search))
+                {
+                    adsQuery = adsQuery
+                        .Where(a => a.Title.ToLower().Contains(search.ToLower()));
+                }
+
+                if (!string.IsNullOrEmpty(locations))
+                {
+                    adsQuery = adsQuery
+                        .Where(a => a.Location.Name.Equals(locations));
+                }
+
+                if (!string.IsNullOrEmpty(category))
+                {
+                    adsQuery = adsQuery
+                        .Where(a => a.Category.Name.Equals(category));
+                }
+
+                ViewBag.CurrentPage = page;
+
+                var ads = adsQuery
+                    .OrderByDescending(a => a.DateCreated)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize)
+                    .Include(a => a.Location)
+                    .Include(a => a.Category)
                     .Include(a => a.Author)
                     .ToList();
 
